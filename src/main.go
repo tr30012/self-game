b4775a6b000bf431b85bcc74a4ab9dc5f6f655eb
tr30012 <72806811+tr30012@users.noreply.github.com>
2021-03-28 12:37:35 +0300

    small refactor

--- src/main.go
+++ src/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"net/http"
+	"os"
 
+	"github.com/gorilla/mux"
+)
+
+const (
+	webAddress = "127.0.0.1:8080"
+)
+
+var (
+	logger *Logger
+	router *mux.Router
+)
+
+func main() {
+	logger = NewLogger(os.Stdout, os.Stdout, os.Stderr, llInfo)
+	logger.Info("Web-address of server is " + webAddress)
+
+	router = mux.NewRouter()
+	router.HandleFunc("/", mainPage)
+
+	if err := http.ListenAndServe(webAddress, router); err != nil {
+		panic(err)
+	}
 }
--- src/models.go
+++ src/models.go
@@ -1,1 +1,9 @@
 package main
+
+type Question struct {
+	Cost     int    `json:"q_cost"`
+	Text     string `json:"q_text"`
+	Answer   string `json:"q_answer"`
+	Event    int    `json:"q_event"`
+	Answered bool   `json:"q_answered"`
+}
